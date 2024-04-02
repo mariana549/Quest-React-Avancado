def17f57793e6396105cc4d6c6171da03c86b19e
Mariana Antonia <marievaristo889@gmail.com>
2024-04-02 16:56:28 -0300

    refatorando

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -2,26 +2,14 @@ import axios from "axios"
 
 export const FetchData = async () => {
    try {
-      // const urls = [];
-      // for (let i = 1; i <= 20; i++) {
-      //    urls.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
-      // }
-      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`)
-      console.log(response)
-
-      const user = await response.data.results;
-      console.log("user",  user) 
-
-      if(!response.ok){
-         throw new Error(`pokeApi returned status code ${response.status}`)
+      const urls = [];
+      for (let i = 1; i <= 10; i++) {      
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+         const data = await response.data; 
+         urls.push(data)
       }
+      console.log(urls) 
    } catch (error) {
       console.error(`Error fetching Pokemon API: ${error.message}`)
    }
-
-   return (
-      <>
-      <p>pokemon: </p>
-      </>
-   )
 }
