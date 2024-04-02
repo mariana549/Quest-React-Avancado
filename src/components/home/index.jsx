50259aabb443644f78ebb6409382ef0f97886fce
Mariana Antonia <marievaristo889@gmail.com>
2024-04-02 16:44:54 -0300

    fazendo a chamada da api com axios

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,5 +1,27 @@
-// import axios from "axios"
+import axios from "axios"
 
-export const FetchData = () => {
+export const FetchData = async () => {
+   try {
+      // const urls = [];
+      // for (let i = 1; i <= 20; i++) {
+      //    urls.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+      // }
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`)
+      console.log(response)
 
+      const user = await response.data.results;
+      console.log("user",  user) 
+
+      if(!response.ok){
+         throw new Error(`pokeApi returned status code ${response.status}`)
+      }
+   } catch (error) {
+      console.error(`Error fetching Pokemon API: ${error.message}`)
+   }
+
+   return (
+      <>
+      <p>pokemon: </p>
+      </>
+   )
 }
