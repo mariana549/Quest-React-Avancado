7cc5cb9ff680b503e6e7fd799e8d5ddb7028a79e
Mariana Antonia <124698724+mariana549@users.noreply.github.com>
2024-11-20 17:19:56 -0300

    fix: :art: corriggindo bugs

--- src/components/Pokemon/abilities/index.jsx
+++ src/components/Pokemon/abilities/index.jsx
@@ -1,35 +1,40 @@
 import { useEffect, useState } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { DescriptionAbilities, Li, Name } from "./styledAbilities";
 import { getAbilty } from "../../../services/requestApi";
 
-export const HabilitiePokemon = ({url, name, type}) => {
-   const [pokemonData, setPokemonData] = useState({});
+export const HabilitiePokemon = ({ name, type }) => {
+  const [pokemonData, setPokemonData] = useState(null);
+  const [habilites, setHabilites] = useState([{ effect: "" }]);
+  
+  useEffect(() => {
+    const getUrl = async () => {
+      const response = await getAbilty(name);
+      setPokemonData(response);
+    };
+    getUrl();
+  }, [name]);
 
-   console.log(pokemonData)
-      // pokemonData não funciona porque tem ter uma forma assincrona de pegar as informações se não, não aparece na tela é da error.
-    // const descriptionAbilitiesEn =  pokemonData.filter(e => e.language.name === "en")
-    
-    useEffect(() => {
-      const getUrl = async () => {
-        const response = await getAbilty(name)
-        setPokemonData(response)
-     }
-    getUrl()
-  }, []);
- 
-   return (
-     <Li>
-       <Name className={type}>{name}: </Name>
-       <DescriptionAbilities> Unable to render description</DescriptionAbilities>
-       {/* <DescriptionAbilities>{pokemonData.map(e => e.effect)}</DescriptionAbilities> */}
-     </Li>
-   );
-}
+  useEffect(() => {
+    if (pokemonData) {
+      const descriptionAbilitiesEn = pokemonData?.filter(
+        (e) => e.language.name === "en"
+      );
+      setHabilites(descriptionAbilitiesEn);
+    }
+  }, [pokemonData]);
 
+  return (
+    <Li>
+      <Name className={type}>{name}: </Name>
+      <DescriptionAbilities>
+        {habilites.map((e) => e?.effect)}
+      </DescriptionAbilities>
+    </Li>
+  );
+};
 
 HabilitiePokemon.propTypes = {
-   name: PropTypes.string.isRequired,
-   url: PropTypes.string.isRequired,
-   type: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.object.isRequired,
 };
