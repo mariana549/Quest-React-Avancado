9887b561ba3da5f231eec0816e370e05599df6be
Mariana Antonia <124698724+mariana549@users.noreply.github.com>
2024-11-21 09:26:01 -0300

    refactor: Modificando os nomes das função de cores do projeto

--- src/utils/constants/constants.jsx
+++ src/utils/constants/constants.jsx
@@ -2,28 +2,26 @@ import Colors from "../colors";
 
 export const limitPokemons = 1302;
 
-export const  mainBg = (props) => props.theme.Theme.mainBg
+export const MainBg = (props) => props.theme.Theme.mainBg;
 
-export const mainPerfilBg = (props) => props.theme.Theme.mainPerfilBg;
+export const MainPerfilBg = (props) => props.theme.Theme.mainPerfilBg;
 
-export const Color0 = (props) => props.theme.Theme.color0
+export const Color0 = (props) => props.theme.Theme.color0;
 
-export const Color10 = (props) => props.theme.Theme.color0
+export const Color10 = (props) => props.theme.Theme.color0;
 
-export const shadow = (props) => Colors.cardBg[props.className.type.name]
+export const Shadow = (props) => Colors.cardBg[props.className.type.name];
 
-export const ColorCardBg = (props) => props.theme.colors.cardBg[props.children]
+export const ColorCardBg = (props) => props.theme.colors.cardBg[props.children];
 
-export const ColorTypes = (props) => props.theme.colors.types[props.children]
+export const ColorTypes = (props) => props.theme.colors.types[props.children];
 
-export const backgroundColorRadial = (props) =>
-   props.theme.Theme.mainBg === "transparent" ?
-      Colors.cardBg[props.className.type.name] :
-      Colors.types[props.className.type.name]
-   ;
+export const BackgroundColorRadial = (props) =>
+  props.theme.Theme.mainBg === "transparent"
+    ? Colors.cardBg[props.className.type.name]
+    : Colors.types[props.className.type.name];
 
 export const ColorTexts = (props) =>
-    props.theme.Theme.mainBg === "transparent" ?
-      Colors.types[props.className.type.name] :
-      Colors.cardBg[props.className.type.name]
-   ;
+  props.theme.Theme.mainBg === "transparent"
+    ? Colors.types[props.className.type.name]
+    : Colors.cardBg[props.className.type.name];
