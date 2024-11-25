2ee55c28f88b318c3bbefb28a72bbc46df8b9cde
Mariana Antonia <124698724+mariana549@users.noreply.github.com>
2024-11-25 13:25:58 -0300

    feat: :wrench: criar e implementar novo pokeProvider
    - Criado novo arquivo `pokeProvider.jsx` para gerenciar o estado global de pokémons - Adicionadas dependências e hooks para buscar dados de pokémons da API - Implementado contexto PokeContext com novos estados e funções para gerenciamento - Substituído provider antigo para melhorar a estrutura e funcionalidade

---
+++ src/contexts/pokeContext/pokeProvider.jsx
@@ -0,0 +1,56 @@
+import axios from "axios";
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import { getPokedex } from '../../services/requestApi/getPokedex';
+import { getPokemon } from '../../services/requestApi/getPokemon';
+import { limitPokemons } from "../../utils/constants/variaveis";
+import PokeContext from "./context";
+
+export const PokeProvider = ({ children }) => {
+  const [pokemons, setPokemons] = useState([]);
+  const [initialPokemons, setInitialPokemons] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [pokemonsVisiveis, setPokemonsVisiveis] = useState(12);
+  const [showButtons, setShowButtons] = useState(true);
+  const pokemonsList = pokemons.slice(0, pokemonsVisiveis);
+
+  useEffect(() => {
+    const fetchInitialPokemons = async () => {
+      try {
+        const data = await getPokedex(limitPokemons, 0);
+        const namesPokemons = data.map((poke) => poke.name);
+        const pokePromises = namesPokemons.map(
+          async (pokeName) => await getPokemon(pokeName)
+        );
+        const pokeDados = await axios.all(pokePromises);
+
+        setInitialPokemons(pokeDados);
+        setPokemons(pokeDados);
+        setLoading(false);
+      } catch (error) {
+        console.error(`Error fetching Pokémon: ${error.message}`);
+      }
+    };
+    fetchInitialPokemons();
+  }, []);
+
+  return (
+    <PokeContext.Provider
+      value={{
+        pokemons,
+        setPokemons,
+        loading,
+        pokemonsList,
+        initialPokemons,
+        showButtons,
+        setShowButtons,
+        pokemonsVisiveis,
+        setPokemonsVisiveis,
+      }}
+    >
+      {children}
+    </PokeContext.Provider>
+  );
+};
+
+PokeProvider.propTypes = { children: PropTypes.node.isRequired };
