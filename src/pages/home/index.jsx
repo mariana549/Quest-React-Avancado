99d5287af193c103e733348c3aecdb8fbc9553cb
Mariana Antonia <124698724+mariana549@users.noreply.github.com>
2024-11-25 13:46:57 -0300

    refactor: :recycle: atuaizar implementação para usar PokeProvider
    - Removidas importações desnecessárias de axios, useEffect e useState - Removido o uso do antigo provider e adicionado o novo PokeProvider - Simplificada a estrutura do componente Home para melhor legibilidade e manutenção

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,55 +1,12 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { PokeHeader } from "../../components/pokeHeader";
 import { PokemonList } from "../../components/pokedex/pokemonList";
-import Provider from "../../contexts/pokeDados/Provider";
-import { getPokedex, getPokemon } from "../../services/requestApi";
+import { PokeProvider } from "../../contexts/pokeContext/pokeProvider";
 
 export default function Home() {
-  const [pokemons, setPokemon] = useState([]);
-  const [pokemonsVisiveis, setPokemonVisiveis] = useState(10);
-  const [loading, setLoading] = useState(true);
-  const [showButtons, setShowButtons] = useState(true);
-  const pokemonsList = pokemons.slice(0, pokemonsVisiveis);
-
-  useEffect(() => {
-    const fetchPokemon = async () => {
-      try {
-        // esse trecho faz uma chamada na api de 10 em 10 pokemons, para não sobrecarregar o site.
-        const data = await getPokedex(pokemonsVisiveis, 0);
-
-        const namesPokemons = data.map((poke) => poke.name);
-
-        const pokePromises = await namesPokemons.map(
-          async (pokeName) => await getPokemon(pokeName)
-        );
-
-        const pokeDados = await axios.all(pokePromises);
-
-        setPokemon([...pokeDados]);
-        setLoading(false);
-      } catch (error) {
-        console.error(`Error fetching Pokemon API: ${error.message}`);
-      }
-    };
-    fetchPokemon();
-  }, [pokemonsVisiveis]);
-
   return (
-    <Provider
-      value={{
-        pokemons: pokemons,
-        loading: loading,
-        pokemonsList: pokemonsList,
-        showButtons: showButtons,
-        pokemonsVisiveis: pokemonsVisiveis,
-        setPokemonVisiveis: setPokemonVisiveis,
-        setPokemon: setPokemon,
-        setShowButtons: setShowButtons,
-      }}
-    >
+    <PokeProvider>
       <PokeHeader />
       <PokemonList />
-    </Provider>
+    </PokeProvider>
   );
 }
