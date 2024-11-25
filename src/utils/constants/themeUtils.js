ef4621cc5967a23bc87e794e9b9d1efca02dc640
Mariana Antonia <124698724+mariana549@users.noreply.github.com>
2024-11-25 14:09:23 -0300

    refactor: :wrench: criado novo arquivos para as funções de temas

---
+++ src/utils/constants/themeUtils.js
@@ -0,0 +1,34 @@
+import Colors from "../colors/index";
+
+export const MainBg = (props) => props.theme.Theme.mainBg;
+
+export const CardBg = (props) =>
+  Colors.cardBg[
+    props.children[0].props.children[2].props.children[0].props.children
+  ];
+
+export const CardDiv = (props) => props.theme.Theme.cardDiv;
+
+export const MainPerfilBg = (props) => props.theme.Theme.mainPerfilBg;
+
+export const Color0 = (props) => props.theme.Theme.color0;
+
+export const Color10 = (props) => props.theme.Theme.color10;
+
+export const Color20 = (props) => props.theme.Theme.color20;
+
+export const Shadow = (props) => Colors.cardBg[props.className.type.name];
+
+export const ColorCardBg = (props) => props.theme.colors.cardBg[props.children];
+
+export const ColorTypes = (props) => props.theme.colors.types[props.children];
+
+export const BackgroundColorRadial = (props) =>
+  props.theme.Theme.mainBg === "transparent"
+    ? Colors.cardBg[props.className.type.name]
+    : Colors.types[props.className.type.name];
+
+export const ColorTexts = (props) =>
+  props.theme.Theme.mainBg === "transparent"
+    ? Colors.types[props.className.type.name]
+    : Colors.cardBg[props.className.type.name];
