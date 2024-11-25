7a577e3fff141c926c851cfb8e663e90c01aa7e9
Mariana Antonia <124698724+mariana549@users.noreply.github.com>
2024-11-25 11:27:55 -0300

    refactor: :recycle: ajustar importações e rotas relativas
    - Alteradas as importações de estilo para arquivos JS - Organizadas as importações para melhor legibilidade - Atualizado o link para a página de perfil de acordo com a atualização do react-router-dom V7

--- src/components/pokedex/pokeCards/index.jsx
+++ src/components/pokedex/pokeCards/index.jsx
@@ -1,39 +1,45 @@
-import PropTypes from 'prop-types'
-import { Box, Div, Id, Item, NomePokemon, PokeCards, PokemonImage, Tipo, TypesList } from './styledCards';
-import { Link } from 'react-router-dom';
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import {
+   Box,
+   Div,
+   Id,
+   Item,
+   NomePokemon,
+   PokeCards,
+   PokemonImage,
+   Tipo,
+   TypesList,
+} from "./styledCards.js";
 
 export default function Card({ pokemon }) {
-   return (
-      <PokeCards>
-         {pokemon.map((poke, i) => {
-            const img = poke.sprites?.other?.["official-artwork"]?.front_default
-            return (
-                  <Link 
-                     to={`Perfil/${poke.name}`}
-                     key={i}
-                     name={poke.name}
-                  >
-                     <Item>
-                        <Box>
-                           <Id>#{poke.id}</Id>
-                           <NomePokemon>{poke.name}</NomePokemon>
-                           <TypesList>
-                              {poke.types.map((type, i) => (
-                                 <Tipo key={i}>{type.type.name}</Tipo>
-                              ))}
-                           </TypesList>
-                        </Box>
-                        <Div>
-                           <PokemonImage src={img} alt={poke.name} />
-                        </Div>
-                     </Item>
-                  </Link>
-            )
-         })}
-      </PokeCards>
-   );
+  return (
+    <PokeCards>
+      {pokemon.map((poke, i) => {
+        const img = poke.sprites?.other?.["official-artwork"]?.front_default;
+        return (
+          <Link to={`../Perfil/${poke.name}`} key={i} name={poke.name}>
+            <Item>
+              <Box>
+                <Id>#{poke.id}</Id>
+                <NomePokemon>{poke.name}</NomePokemon>
+                <TypesList>
+                  {poke.types.map((type, i) => (
+                    <Tipo key={i}>{type.type.name}</Tipo>
+                  ))}
+                </TypesList>
+              </Box>
+              <Div>
+                <PokemonImage src={img} alt={poke.name} />
+              </Div>
+            </Item>
+          </Link>
+        );
+      })}
+    </PokeCards>
+  );
 }
 
 Card.propTypes = {
-   pokemon: PropTypes.array.isRequired,
+  pokemon: PropTypes.array.isRequired,
 };
