0c7c4aa8f6de532e221867db12f6d1cecdf5a00d
Mariana Antonia <124698724+mariana549@users.noreply.github.com>
2024-11-26 16:15:36 -0300

    🧪 test: adicionar testes para o componente PokemonList
    - Adicionado teste para verificar a renderização correta do componente sem dados
    - Adicionado teste para verificar a renderização correta do componente com dados de Pokémon
    - Adicionado teste para verificar a exibição de texto alternativo 'loading...'
    - Adicionado teste para verificar a exibição do botão "Search More"
    - Adicionado teste para verificar a exibição do botão "Go up to"
    - Adicionado teste para verificar a atualização do estado de showButtons

---
+++ src/components/pokedex/pokemonList/pokemonList.test.jsx
@@ -0,0 +1,188 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { PokemonList } from ".";
+import PokeContext from "../../../contexts/pokeContext/context";
+import Colors from "../../../utils/colors";
+import { Themes } from "../../../utils/themes";
+
+const mockContextValue = {
+  pokemons: [],
+  loading: false,
+  pokemonsList: [],
+  showButtons: false,
+  setPokemonsVisiveis: jest.fn(),
+  setShowButtons: jest.fn(),
+};
+describe("PokemonList", () => {
+  const theme = Themes;
+  it("deve renderizar corretamente o componente", () => {
+    render(
+      <Router>
+        <ThemeProvider theme={{ Theme: theme, colors: Colors }}>
+          <PokeContext.Provider value={mockContextValue}>
+            <PokemonList />
+          </PokeContext.Provider>
+        </ThemeProvider>
+      </Router>
+    );
+
+    expect(
+      screen.getByText(/pokemons not found or undefined/i)
+    ).toBeInTheDocument();
+  });
+
+  it("deve renderizar corretamente com os dados do pokemon", () => {
+    const valueWithPokemons = {
+      ...mockContextValue,
+      pokemons: [{ id: 1, name: "bulbasaur" }],
+      pokemonsList: [
+        {
+          id: 1,
+          name: "bulbasaur",
+          sprites: {
+            other: {
+              "official-artwork": {
+                front_default: "https://example.com/bulbasaur.png",
+              },
+            },
+          },
+          types: [{ type: { name: "grass" } }, { type: { name: "poison" } }],
+        },
+      ],
+    };
+    render(
+      <Router>
+        <ThemeProvider theme={{ Theme: Themes, colors: Colors }}>
+          <PokeContext.Provider value={valueWithPokemons}>
+            <PokemonList />
+          </PokeContext.Provider>
+        </ThemeProvider>
+      </Router>
+    );
+    expect(screen.getByText(/bulbasaur/i)).toBeInTheDocument();
+  });
+
+  it("deve exibir o texto alternativo 'loanding...'", () => {
+    render(
+      <Router>
+        <ThemeProvider theme={{ Theme: Themes, colors: Colors }}>
+          <PokeContext.Provider value={{ ...mockContextValue, loading: true }}>
+            <PokemonList />
+          </PokeContext.Provider>
+        </ThemeProvider>
+      </Router>
+    );
+
+    expect(
+      screen.getByRole("alert", { name: /loanding.../i })
+    ).toBeInTheDocument();
+  });
+
+  it("deve exibir o texto alternativo 'loanding...'", () => {
+    render(
+      <Router>
+        <ThemeProvider theme={{ Theme: Themes, colors: Colors }}>
+          <PokeContext.Provider value={{ ...mockContextValue, loading: true }}>
+            <PokemonList />
+          </PokeContext.Provider>
+        </ThemeProvider>
+      </Router>
+    );
+
+    expect(
+      screen.getByRole("alert", { name: /loanding.../i })
+    ).toBeInTheDocument();
+  });
+
+  it('Deve exibir o botão "Search More"', () => {
+    const valueWithShowButtons = {
+      ...mockContextValue,
+      showButtons: true,
+      pokemonsList: [
+        {
+          id: 1,
+          name: "bulbasaur",
+          sprites: {
+            other: {
+              "official-artwork": {
+                front_default: "https://example.com/bulbasaur.png",
+              },
+            },
+          },
+          types: [{ type: { name: "grass" } }, { type: { name: "poison" } }],
+        },
+      ],
+      pokemons: [
+        { id: 1, name: "bulbasaur" },
+        { id: 2, name: "ivysaur" },
+      ],
+    };
+    render(
+      <Router>
+        <ThemeProvider theme={{ Theme: Themes, colors: Colors }}>
+          <PokeContext.Provider value={valueWithShowButtons}>
+            <PokemonList />
+          </PokeContext.Provider>
+        </ThemeProvider>
+      </Router>
+    );
+    expect(screen.getByText(/search more/i)).toBeInTheDocument();
+  });
+  it('Deve exibir o botão "Go up to"', () => {
+    const valueWithShowButtons = {
+      ...mockContextValue,
+      showButtons: true,
+      pokemonsList: [
+        {
+          id: 1,
+          name: "bulbasaur",
+          sprites: {
+            other: {
+              "official-artwork": {
+                front_default: "https://example.com/bulbasaur.png",
+              },
+            },
+          },
+          types: [{ type: { name: "grass" } }, { type: { name: "poison" } }],
+        },
+      ],
+      pokemons: [
+        { id: 1, name: "bulbasaur" },
+      ],
+    };
+    render(
+      <Router>
+        <ThemeProvider theme={{ Theme: Themes, colors: Colors }}>
+          <PokeContext.Provider value={valueWithShowButtons}>
+            <PokemonList />
+          </PokeContext.Provider>
+        </ThemeProvider>
+      </Router>
+    );
+    expect(screen.getByText(/go up to/i)).toBeInTheDocument();
+  });
+
+  it("Deve atualizar o estado de showButtons corretamente", () => {
+    const mockSetShowButtons = jest.fn();
+    const valueWithPokemons = {
+      ...mockContextValue,
+      pokemons: [{ id: 1, name: "bulbasaur" }],
+      setShowButtons: mockSetShowButtons,
+    };
+
+    render(
+      <Router>
+        <ThemeProvider theme={{ Theme: Themes, colors: Colors }}>
+          <PokeContext.Provider value={valueWithPokemons}>
+            <PokemonList />
+          </PokeContext.Provider>
+        </ThemeProvider>
+      </Router>
+    );
+
+    expect(mockSetShowButtons).toHaveBeenCalledWith(true);
+  });
+
+
+});
