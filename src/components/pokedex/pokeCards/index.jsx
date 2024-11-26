d54eafdc046f5bcb00ffb0f42abd49a3f92686df
Mariana Antonia <124698724+mariana549@users.noreply.github.com>
2024-11-26 13:31:14 -0300

    style: :lipstick: atualização de indentação

--- src/components/pokedex/pokeCards/index.jsx
+++ src/components/pokedex/pokeCards/index.jsx
@@ -1,15 +1,15 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import {
-   Box,
-   Div,
-   Id,
-   Item,
-   NomePokemon,
-   PokeCards,
-   PokemonImage,
-   Tipo,
-   TypesList,
+  Box,
+  Div,
+  Id,
+  Item,
+  NomePokemon,
+  PokeCards,
+  PokemonImage,
+  Tipo,
+  TypesList,
 } from "./styledCards.js";
 
 export default function Card({ pokemon }) {
