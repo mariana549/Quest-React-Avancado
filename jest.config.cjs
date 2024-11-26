e16c3a8d07ff535f92f5a7dd39f3b53cbd802069
Mariana Antonia <124698724+mariana549@users.noreply.github.com>
2024-11-26 16:05:39 -0300

    🔧 chore: atualizar configuração do Jest para suportar arquivos de mídia e bibliotecas específicas
    - Adicionado mapeamento de módulo para arquivos de mídia (png, jpg, jpeg, gif, svg)
    - Adicionado transformIgnorePatterns para incluir bibliotecas específicas

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,8 +3,10 @@ module.exports = {
   testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(png|jpg|jpeg|gif|svg)$": "<rootDir>/__mocks__/fileMock.js",
   },
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
   },
+  transformIgnorePatterns: ["/node_modules/(?!(your-module|another-module)/)"],
 };
