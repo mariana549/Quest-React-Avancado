0c16b710e355cc36c54db1f5c05bb6915414806c
Mariana Antonia <124698724+mariana549@users.noreply.github.com>
2024-11-28 11:16:28 -0300

    feat: :sparkles: add new resources
    -adicionado novo paramentro setPixels - adicionado nova funcionalidade de setar o valor dos pixels atual

--- src/functions/handleShowMore.js
+++ src/functions/handleShowMore.js
@@ -1,9 +1,11 @@
 import { limitPokemons } from "../utils/constants/variaveis";
 
-export const handlerShowMore = (setPokemonsVisiveis) => {
+export const handlerShowMore = (setPokemonsVisiveis, setPixels) => {
   setPokemonsVisiveis((pokemonsVisiveis) =>
     pokemonsVisiveis + 12 <= limitPokemons
       ? pokemonsVisiveis + 12
       : limitPokemons
   );
+
+  setPixels(window.scrollY)
 };
