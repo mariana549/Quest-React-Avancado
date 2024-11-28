9636f6b7d671aa8e226c94b4cdf697f6d05d3596
Mariana Antonia <124698724+mariana549@users.noreply.github.com>
2024-11-28 11:07:30 -0300

    refactor: :recycle: atualizações nas importações  e alterações na indentação do codigo
    - alterado a importação do contexto para 'pokeContext' ao invés de 'context'. - alterando a ordem dos pokeNomes - adicionando um alt mais descritivo de acordo com os nomes dos pokemons

--- src/components/pokeHeader/index.jsx
+++ src/components/pokeHeader/index.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import logoPokemon from "../../assets/image/pokemon-logo.png";
-import PokeContext from "../../contexts/pokeContext/context";
-import { getPokemonSpritesByName } from '../../functions/getPokemonSpritesByName.js';
+import PokeContext from "../../contexts/pokeContext/pokeContext";
+import { getPokemonSpritesByName } from "../../functions/getPokemonSpritesByName.js";
 import PokeSearch from "../../utils/pokeFilters/pokeSearch/index";
 import PokeTypesFiltered from "../../utils/pokeFilters/pokeTypes/PokeTypesFiltered";
 import Container from "../container/index";
@@ -10,29 +10,31 @@ import { Box, BoxsContainer, Div, Header, Logo } from "./styledHeader.js";
 export const PokeHeader = () => {
   const { pokemons } = useContext(PokeContext);
 
-  const pokeNames = ["pikachu", "charmander", "bulbasaur", "squirtle"];
+  const pokeNames = ["bulbasaur", "charmander", "squirtle", "pikachu"];
   const pokeImageFiltered = getPokemonSpritesByName(pokemons, pokeNames);
 
   return (
-    <>
-      <Header>
-        <Container>
-          <Div>
-            <Logo src={logoPokemon} alt="Logo" />
-            <BoxsContainer>
-              <Box>
-                {pokeImageFiltered.map((gif, i) => (
-                  <img src={gif} key={i} alt="" />
-                ))}
-              </Box>
-              <Box>
-                <PokeSearch />
-                <PokeTypesFiltered />
-              </Box>
-            </BoxsContainer>
-          </Div>
-        </Container>
-      </Header>
-    </>
+    <Header>
+      <Container>
+        <Div>
+          <Logo src={logoPokemon} alt="Logo" />
+          <BoxsContainer>
+            <Box>
+              {pokeImageFiltered.map((gif, i) => (
+                <img
+                  src={gif}
+                  key={i}
+                  alt={`Animated image of Pokémon ${pokeNames[i]}`}
+                />
+              ))}
+            </Box>
+            <Box>
+              <PokeSearch />
+              <PokeTypesFiltered />
+            </Box>
+          </BoxsContainer>
+        </Div>
+      </Container>
+    </Header>
   );
 };
