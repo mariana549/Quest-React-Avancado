f464c435a14c018eb667d26b829c42acd6ea6011
Mariana Antonia <124698724+mariana549@users.noreply.github.com>
2024-11-28 11:25:46 -0300

    refactor: :label: add label
    - adicionado label ao component - alterando logica do componente adicionado o setPixel(0) - atualizando impotação do contexto - alterando o plcaceholder

--- src/utils/pokeFilters/pokeSearch/index.jsx
+++ src/utils/pokeFilters/pokeSearch/index.jsx
@@ -1,15 +1,16 @@
 import { useContext } from "react";
-import PokeContext from "../../../contexts/pokeContext/context";
+import PokeContext from "../../../contexts/pokeContext/pokeContext";
 import { Pesquisar } from "./styledSearch.js";
 
 function PokeSearch() {
-  const { setPokemons, initialPokemons, setPokemonsVisiveis } =
+  const { setPokemons, initialPokemons, setPokemonsVisiveis, setPixels } =
     useContext(PokeContext);
 
   const getPokemonsSearch = (name) => {
     if (name === "") {
       setPokemons(initialPokemons);
       setPokemonsVisiveis(12);
+      setPixels(0);
     } else {
       const filteredPokemons = initialPokemons.filter(({ name: pokeName }) =>
         pokeName.toLowerCase().includes(name.toLowerCase())
@@ -17,14 +18,15 @@ function PokeSearch() {
 
       setPokemons(filteredPokemons);
       setPokemonsVisiveis(12);
+      setPixels(0);
     }
   };
 
   return (
     <form>
       <Pesquisar
-        placeholder="Pesquisando..."
-        className="pesquisando"
+        placeholder="Search..."
+        aria-label="Search pokemon"
         onChange={(event) => getPokemonsSearch(event.target.value)}
       />
     </form>
