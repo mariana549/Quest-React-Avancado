aed2b9a3915ec5633e0c43de88a7626052d85b70
Mariana Antonia <124698724+mariana549@users.noreply.github.com>
2024-11-28 11:27:45 -0300

    refactor: :label: adicionado label e atualizando importações
    - adicionado label em todos os options e select - alterando logica do projeto adicionando o setPixel - alterando importação do contexto

--- src/utils/pokeFilters/pokeTypes/PokeTypesFiltered.jsx
+++ src/utils/pokeFilters/pokeTypes/PokeTypesFiltered.jsx
@@ -1,15 +1,16 @@
 import { useContext } from "react";
-import PokeContext from "../../../contexts/pokeContext/context";
+import PokeContext from "../../../contexts/pokeContext/pokeContext";
 import { Option, SelectTypes } from "./styledTypes.js";
 
 function PokeTypesFiltered() {
-  const { setPokemons, initialPokemons, setPokemonsVisiveis } =
+  const { setPokemons, initialPokemons, setPokemonsVisiveis, setPixels } =
     useContext(PokeContext);
 
   const getPokemonsTypes = (typeName) => {
     if (typeName === "") {
       setPokemons(initialPokemons);
       setPokemonsVisiveis(12);
+      setPixels(0);
     } else {
       const filteredPokemons = initialPokemons.filter(({ types }) =>
         types.some(({ type: { name } }) => name === typeName)
@@ -17,30 +18,72 @@ function PokeTypesFiltered() {
 
       setPokemons(filteredPokemons);
       setPokemonsVisiveis(12);
+      setPixels(0);
     }
   };
 
   return (
-    <SelectTypes onChange={(event) => getPokemonsTypes(event.target.value)}>
-      <Option value="">- All -</Option>
-      <Option value="bug">Bug</Option>
-      <Option value="dark">Dark</Option>
-      <Option value="dragon">Dragon</Option>
-      <Option value="electric">Electric</Option>
-      <Option value="fairy">Fairy</Option>
-      <Option value="fighting">Fighting</Option>
-      <Option value="fire">Fire</Option>
-      <Option value="flying">Flying</Option>
-      <Option value="ghost">Ghost</Option>
-      <Option value="ground">Ground</Option>
-      <Option value="ice">Ice</Option>
-      <Option value="normal">Normal</Option>
-      <Option value="poison">Poison</Option>
-      <Option value="psychic">Psychic</Option>
-      <Option value="rock">Rock</Option>
-      <Option value="steel">Steel</Option>
-      <Option value="water">Water</Option>
-      <Option value="grass">Grass</Option>
+    <SelectTypes
+      onChange={(event) => getPokemonsTypes(event.target.value)}
+      aria-label="Filter Pokémon by type"
+    >
+      <Option value="" aria-label="all types">
+        - All -
+      </Option>
+      <Option value="bug" aria-label="type bug">
+        Bug
+      </Option>
+      <Option value="dark" aria-label="type dark">
+        Dark
+      </Option>
+      <Option value="dragon" aria-label="type dragon">
+        Dragon
+      </Option>
+      <Option value="electric" aria-label="type electric">
+        Electric
+      </Option>
+      <Option value="fairy" aria-label="type fairy">
+        Fairy
+      </Option>
+      <Option value="fighting" aria-label="type fighting">
+        Fighting
+      </Option>
+      <Option value="fire" aria-label="type fire">
+        Fire
+      </Option>
+      <Option value="flying" aria-label="type flying">
+        Flying
+      </Option>
+      <Option value="ghost" aria-label="type ghost">
+        Ghost
+      </Option>
+      <Option value="ground" aria-label="type ground">
+        Ground
+      </Option>
+      <Option value="ice" aria-label="type ice">
+        Ice
+      </Option>
+      <Option value="normal" aria-label="type normal">
+        Normal
+      </Option>
+      <Option value="poison" aria-label="type poison">
+        Poison
+      </Option>
+      <Option value="psychic" aria-label="type psychic">
+        Psychic
+      </Option>
+      <Option value="rock" aria-label="type rock">
+        Rock
+      </Option>
+      <Option value="steel" aria-label="type steel">
+        Steel
+      </Option>
+      <Option value="water" aria-label="type water">
+        Water
+      </Option>
+      <Option value="grass" aria-label="type grass">
+        Grass
+      </Option>
     </SelectTypes>
   );
 }
